000f5355d5a7e47b22c5000777323681a7ea8260
Mario Martinez <mariojaimemartinez27@gmail.com>
2024-05-21 20:15:27 -0600

    Creacion de componentes Game y Turn

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,16 @@
 import "./App.css";
-import Square from "./components/Square";
 import confetti from "canvas-confetti";
 import { useState } from "react";
 import { TURNS } from "./constants";
 import { checkWinner, checkEndGame } from "./logic/board";
 import { WinnerModal } from "./components/WinnerModal";
+import { Game } from "./components/Game";
+import { Turn } from "./components/Turn";
 
 function App() {
   const [board, setBoard] = useState(Array(9).fill(null));
   const [turn, setTurn] = useState(TURNS.X);
   const [winner, setWinner] = useState(null);
-
-  const resetGame = () => {
-    setBoard(Array(9).fill(null));
-    setTurn(TURNS.X);
-    setWinner(null);
-  };
 
   const updateBoard = (index) => {
     if (board[index] || winner) return;
@@ -36,25 +31,18 @@ function App() {
     }
   };
 
+  const resetGame = () => {
+    setBoard(Array(9).fill(null));
+    setTurn(TURNS.X);
+    setWinner(null);
+  };
+
   return (
     <main className="board">
       <h1>Equis Cero</h1>
-
-      <section className="game">
-        {board.map((square, index) => {
-          return (
-            <Square key={index} index={index} updateBoard={updateBoard}>
-              {square}
-            </Square>
-          );
-        })}
-      </section>
-
-      <section className="turn">
-        <Square isSelected={turn === TURNS.X}>{TURNS.X}</Square>
-        <Square isSelected={turn === TURNS.O}>{TURNS.O}</Square>
-      </section>
-
+      <button onClick={resetGame}>Jugar de Nuevo</button>
+      <Game board={board} updateBoard={updateBoard}></Game>
+      <Turn turn={turn}></Turn>
       <WinnerModal resetGame={resetGame} winner={winner}></WinnerModal>
     </main>
   );
